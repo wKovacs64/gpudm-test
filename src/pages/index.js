ae77dff51e1235cc9288f2b8fb4716bc2dab5f19
Justin Hall <justin.r.hall@gmail.com>
2019-08-08 21:12:00 -0600

    feat: add dark mode

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,29 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import useDarkMode from "use-dark-mode"
+import Switch from "react-switch"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  const { value, toggle } = useDarkMode(false)
 
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div>
+        <span>Toggle dark mode:</span>{" "}
+        <Switch checked={value} onChange={toggle} />
+      </div>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  )
+}
 export default IndexPage
